8300cf5f0a7143acb0b0ddba0af42e76812f19fa
unknown <sarahmm24@gmail.com>
2025-09-23 22:40:34 -0700

    Updated Navbar and made sandwich button functional with dropdown nav links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,53 @@
-import Link from 'next/link'
+import { useState } from "react";
+import Link from 'next/link';
 import Image from 'next/image'
 import styles from "@/styles/Navbar.module.css";
 
 export default function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
         <>
-            <nav className={styles.navbar}>
+            <nav className={styles.navBar}>
 
-                <div className={styles.topnav}>
+                <div className={styles.topNav}>
                     <div className={styles.logo}>Syrena and George</div>
 
-                    <button className={styles.navbutton}><Image src="/nav-icon.svg" width="25" height="25"/></button>
+                    <button 
+                        className={styles.navButton}
+                        onClick={() => setMenuOpen(!menuOpen)}
+                    >
+                        <Image src="/nav-icon.svg" width="25" height="25"/>
+                    </button>
                 </div>
+           
+                <ul className={styles.navLinks}>
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/rsvp">RSVP</Link></li>
+                    <li><Link href="/">Wedding Details</Link></li>
+                    <li><Link href="/">Wedding Attire</Link></li>
+                    <li><Link href="/">Travel</Link></li>
+                    <li><Link href="/">Activities</Link></li>
+                    <li><Link href="/">Activities</Link></li>
+                    <li><Link href="/">Registry</Link></li>
+                </ul>
 
-                <div className={styles.bottomnav}>                
-                    <ul className={styles.navlinks}>
-                        <li><Link href="/">Home</Link></li>
-                        <li><Link href="/rsvp">RSVP</Link></li>
-                        <li><Link href="/">Wedding Details</Link></li>
-                        <li><Link href="/">Wedding Attire</Link></li>
-                        <li><Link href="/">Travel</Link></li>
-                        <li><Link href="/">Activities</Link></li>
-                        <li><Link href="/">Activities</Link></li>
-                        <li><Link href="/">Registry</Link></li>
-                    </ul>
-                </div>
+                {menuOpen && (
+                    <div className={styles.mobileMenu}>
+                        <ul className={styles.mobileNavlinks}>
+                            <li><Link href="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
+                            <li><Link href="/rsvp" onClick={() => setMenuOpen(false)}>RSVP</Link></li>
+                            <li><Link href="/" onClick={() => setMenuOpen(false)}>Wedding Details</Link></li>
+                            <li><Link href="/" onClick={() => setMenuOpen(false)}>Wedding Attire</Link></li>
+                            <li><Link href="/" onClick={() => setMenuOpen(false)}>Travel</Link></li>
+                            <li><Link href="/" onClick={() => setMenuOpen(false)}>Activities</Link></li>
+                            <li><Link href="/" onClick={() => setMenuOpen(false)}>Activities</Link></li>
+                            <li><Link href="/" onClick={() => setMenuOpen(false)}>Registry</Link></li>
+                        </ul>
+                        <button className={styles.closeButton} onClick={() => setMenuOpen(false)}>
+                            <Image src="/close-icon.svg" width="25" height="25"/>
+                        </button>
+                    </div>
+                )}
             </nav>
         </>
     )
