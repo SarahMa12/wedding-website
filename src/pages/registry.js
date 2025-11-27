064160a661fe6bfa61cdf945cff28730c01208b9
Sarah Ma <sarahma@Mac.attwifi.manager>
2025-11-27 14:18:20 -0800

    Image for registry

--- src/pages/registry.js
+++ src/pages/registry.js
@@ -1,14 +1,27 @@
+import Image from "next/image";
 import styles from "@/styles/Registry.module.css";
 
 export default function Registry() {
   return (
     <div className={styles.registryPage}>
+      
+      {/* Top Hero Image */}
+      <div className={styles.topImageWrapper}>
+        <Image
+          src="/images/registry.JPG"
+          alt="Registry"
+          layout="fill"
+          objectFit="cover"
+          priority
+        />
+      </div>
+
       <div className={styles.registryInner}>
         <div className={styles.registryCard}>
           <h1 className={styles.pageTitle}>Registry</h1>
           <p className={styles.pageSubheading}>
             We&apos;re putting together something meaningful for our registry.
-            More information will be available soon. ♡
+            More information will be available soon.
           </p>
         </div>
       </div>
