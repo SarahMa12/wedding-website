2343c21d1f8223a128d899b05fe7be3baa78115e
Sarah Ma <sarahma@Mac.attwifi.manager>
2025-11-27 13:56:56 -0800

    remove navbar from pwd gate

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -40,7 +40,6 @@ export default function App({ Component, pageProps }) {
   if (isProtected && !unlocked) {
     return (
       <>
-        <Navbar />
         <PasswordGate onUnlock={() => setUnlocked(true)} />
       </>
     );
